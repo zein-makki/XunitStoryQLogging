6b6fed717be2298635b1c457cab107ed6aac3bd2
Zein Makki <zein.makki@gotocme.com>
2018-11-07 22:59:24 +0200

    Initial Commit

---
+++ Xunit.StoryQ.Tests/SampleTest.cs
@@ -0,0 +1,32 @@
+﻿using StoryQ;
+
+namespace Xunit.StoryQ.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void StoryQTest1()
+        {
+            Feature story = new Story("Feature 1")
+                .InOrderTo("Do something 1")
+                .AsA("user")
+                .IWant("Verify feature one");
+
+            story.WithScenario("Given When Then Test 1")
+                .Given(GivenMethod)
+                .When(WhenMethod)
+                .Then(ThenMethod);
+
+            story.WithScenario("Given When Then Test 2")
+                .Given(GivenMethod)
+                .When(WhenMethod)
+                .Then(ThenMethod);
+
+            story.Execute();
+        }
+
+        private void GivenMethod() { }
+        private void WhenMethod() { }
+        private void ThenMethod() { }
+    }
+}
